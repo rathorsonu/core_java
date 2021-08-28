7adfd36d0d801d1b22fa96467eea7b2e0b82d671
rathorsonu <sunnysonurathor2016@gamil.com>
2021-08-28 00:25:09 -0700

    test

--- count_java/src/count_java/Test.java
+++ count_java/src/count_java/Test.java
@@ -3,7 +3,9 @@ package count_java;
 public class Test {
 	public static void main(String []args) {
 		System.out.println("Hello java");
-		System.out.println(" aa");
+for(int i=1;i<=10;i++) {
+	System.out.println("hello sonu");
+}
 	
 	}
 
